403cdaa17e46957dba7a6d4ff5c0502e1e8a67d4
Prashant Chandraker <prashantchandraker1@gmail.com>
2025-03-19 15:03:40 +0530

    changes

--- lib/model/category_model.dart
+++ lib/model/category_model.dart
@@ -1,4 +1,5 @@
 class CategoryModel {
   String? categoryName;
   String? image;
+  CategoryModel({required this.categoryName, required this.image});
 }
--- lib/services/data.dart
+++ lib/services/data.dart
@@ -1,33 +1,12 @@
 import 'package:news_app/model/category_model.dart';
 
 List<CategoryModel> getCategories() {
-  List<CategoryModel> category = [];
-  CategoryModel categoryModel = CategoryModel();
-
-  categoryModel.categoryName = "Bussiness";
-  categoryModel.image = "assets/bussiness_box.jpeg";
-  category.add(categoryModel);
-  categoryModel = CategoryModel();
-
-  categoryModel.categoryName = "Entertainment";
-  categoryModel.image = "assets/entertainment.jpeg";
-  category.add(categoryModel);
-  categoryModel = CategoryModel();
-
-  categoryModel.categoryName = "Science";
-  categoryModel.image = "assets/science.jpeg";
-  category.add(categoryModel);
-  categoryModel = CategoryModel();
-
-  categoryModel.categoryName = "Sports";
-  categoryModel.image = "assets/sports.jpeg";
-  category.add(categoryModel);
-  categoryModel = CategoryModel();
-
-  categoryModel.categoryName = "Technology";
-  categoryModel.image = "assets/technology.jpeg";
-  category.add(categoryModel);
-  categoryModel = CategoryModel();
-
-  return category;
+  return [
+    CategoryModel(categoryName: "Business", image: "assets/bussiness_box.jpeg"),
+    CategoryModel(
+        categoryName: "Entertainment", image: "assets/entertainment.jpeg"),
+    CategoryModel(categoryName: "Science", image: "assets/science.jpeg"),
+    CategoryModel(categoryName: "Sports", image: "assets/sports.jpeg"),
+    CategoryModel(categoryName: "Technology", image: "assets/technology.jpeg"),
+  ];
 }
